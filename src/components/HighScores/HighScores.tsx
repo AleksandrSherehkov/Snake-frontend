b817b86dd00456ff5d59cdbc480dfba474e147b1
Aleksandr Shereshkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2024-07-08 10:08:13 +0300

    Update HighScores.tsx

--- src/components/HighScores/HighScores.tsx
+++ src/components/HighScores/HighScores.tsx
@@ -1,10 +1,13 @@
 import { FC, memo } from 'react';
 import { Score } from '../../types/definitions';
 import { Loader } from '../Loader/Loader';
+import { TrophyIcon } from '@heroicons/react/24/solid';
 
 interface HighScoresProps {
   highScores: Score[];
 }
+
+const trophyColors = ['text-yellow-500', 'text-gray-400', 'text-orange-700'];
 
 export const HighScores: FC<HighScoresProps> = memo(({ highScores }) => {
   return (
@@ -14,8 +17,18 @@ export const HighScores: FC<HighScoresProps> = memo(({ highScores }) => {
         <Loader />
       ) : (
         <ol className="list-decimal flex flex-col items-center gap-1 ">
-          {highScores.map(score => (
-            <li key={score.id} className="font-semibold text-red-500">
+          {highScores.map((score, index) => (
+            <li
+              key={score.id}
+              className="font-semibold text-red-500 flex items-center gap-2"
+            >
+              {index < 3 ? (
+                <TrophyIcon className={`${trophyColors[index]} w-4 h-4`} />
+              ) : (
+                <span className="text-blue-500 font-semibold">
+                  {index + 1}.
+                </span>
+              )}
               <span className="text-blue-500 font-semibold">
                 {score.name}:{' '}
               </span>
